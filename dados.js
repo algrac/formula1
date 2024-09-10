cb8998fc5e1f87751084781a0a1d473bc1b3e2f9
algrac <alexandre.gracianofilho@gmail.com>
2024-09-09 23:04:24 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+      titulo: "Ayrton Senna",
+      descricao: "Ayrton Senna da Silva foi mais do que um piloto de Fórmula 1, ele se tornou um símbolo nacional e um ícone do esporte mundial. Sua habilidade excepcional ao volante, aliada a uma determinação inigualável, o levaram a conquistar três títulos mundiais e a conquistar o coração de milhões de fãs ao redor do globo. Além de suas vitórias nas pistas, Senna era conhecido por sua paixão pelo Brasil, sua humildade e sua luta por causas sociais. Sua morte prematura, em 1994, deixou um vazio no esporte e no coração dos brasileiros, mas seu legado continua vivo, inspirando novas gerações de atletas e fãs.",
+      link: "https://en.wikipedia.org/wiki/Ayrton_Senna",
+      tags: "tri piloto brasileiro"
+    },
+    {
+      titulo: "Lewis Hamilton",
+      descricao: "Lewis Hamilton é um dos pilotos de Fórmula 1 mais bem-sucedidos da história, com múltiplos títulos mundiais e inúmeras vitórias. Conhecido por sua velocidade e habilidade técnica, Hamilton também é um ativista social, utilizando sua plataforma para promover a diversidade e a igualdade. Sua rivalidade com outros grandes pilotos, como Max Verstappen, tornou-o uma figura ainda mais icônica no mundo do automobilismo.",
+      link: "https://en.wikipedia.org/wiki/Lewis_Hamilton",
+      tags: "hepta piloto inglês"
+    },
+    {
+      titulo: "Max Verstappen",
+      descricao: "Max Verstappen emergiu como uma das maiores promessas da Fórmula 1 desde muito jovem. Com um estilo de pilotagem agressivo e uma determinação inabalável, Verstappen conquistou múltiplos títulos mundiais e se tornou um dos pilotos mais populares da atualidade. Sua rivalidade com Lewis Hamilton, marcada por momentos de grande intensidade nas pistas, o consolidou como um dos maiores nomes do esporte.",
+      link: "https://en.wikipedia.org/wiki/Max_Verstappen",
+      tags: "tri piloto holandes"
+    }
+  ];
